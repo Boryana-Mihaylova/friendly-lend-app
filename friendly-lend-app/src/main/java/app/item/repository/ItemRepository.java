78bf77000b65776b5f1cdd1f731af802dd7d72c9
Boryana Mihaylova <130145291+BoryanaMihaylova@users.noreply.github.com>
2025-07-14 19:06:43 +0300

    add query to find items by status and createdAt before threshold

--- friendly-lend-app/src/main/java/app/item/repository/ItemRepository.java
+++ friendly-lend-app/src/main/java/app/item/repository/ItemRepository.java
@@ -3,9 +3,11 @@ package app.item.repository;
 
 
 import app.item.model.Item;
+import app.item.model.ItemStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 @Repository
@@ -14,5 +16,7 @@ public interface ItemRepository extends JpaRepository<Item, UUID> {
     List<Item> findByOwnerId(UUID ownerId);
 
     List<Item> findAllByOwnerIdAndBorrowerIsNull(UUID ownerId);
+
+    List<Item> findAllByStatusAndCreatedAtBefore(ItemStatus status, Date createdBefore);
 
 }

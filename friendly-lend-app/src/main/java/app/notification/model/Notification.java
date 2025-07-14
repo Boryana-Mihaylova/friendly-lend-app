eea630fd9f1a2b99095143617ba189b0ff577c8e
Boryana Mihaylova <130145291+BoryanaMihaylova@users.noreply.github.com>
2025-07-14 19:00:15 +0300

    add Notification entity

---
+++ friendly-lend-app/src/main/java/app/notification/model/Notification.java
@@ -0,0 +1,34 @@
+package app.notification.model;
+
+import app.user.model.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Notification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne(optional = false)
+    private User user;
+
+    @Column(nullable = false)
+    private String message;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(nullable = false, updatable = false)
+    private Date createdAt;
+
+    @Column(name = "`read`", nullable = false)
+    private boolean read;
+}

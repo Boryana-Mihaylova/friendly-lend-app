f8036294b9e88c37f2f7e911c918e0d3ba2f0573
Boryana Mihaylova <130145291+BoryanaMihaylova@users.noreply.github.com>
2025-07-15 18:39:25 +0300

    add and fix IndexController API tests with working dependencies

--- friendly-lend-app/src/test/java/app/web/IndexControllerApiTest.java
+++ friendly-lend-app/src/test/java/app/web/IndexControllerApiTest.java
@@ -3,6 +3,7 @@ package app.web;
 import app.exception.UsernameAlreadyExistException;
 import app.item.service.ItemService;
 import app.security.AuthenticationMetadata;
+import app.survey.service.SurveyService;
 import app.user.model.UserRole;
 import app.user.service.UserService;
 import app.web.dto.RegisterRequest;
@@ -10,7 +11,7 @@ import app.web.mapper.IndexController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
@@ -19,7 +20,6 @@ import java.util.UUID;
 import static app.web.TestBuilder.aRandomUser;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
-
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -29,33 +29,28 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(IndexController.class)
 public class IndexControllerApiTest {
 
-
-    @MockitoBean
+    @MockBean
     private UserService userService;
-    @MockitoBean
+
+    @MockBean
     private ItemService itemService;
 
+    @MockBean
+    private SurveyService surveyService;
 
     @Autowired
     private MockMvc mockMvc;
 
-
     @Test
     void getRequestToIndexEndpoint_shouldReturnIndexView() throws Exception {
-
-        MockHttpServletRequestBuilder request = get("/");
-
-        mockMvc.perform(request)
+        mockMvc.perform(get("/"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("index"));
     }
 
     @Test
     void getRequestToRegisterEndpoint_shouldReturnRegisterView() throws Exception {
-
-        MockHttpServletRequestBuilder request = get("/register");
-
-        mockMvc.perform(request)
+        mockMvc.perform(get("/register"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("register"))
                 .andExpect(model().attributeExists("registerRequest"));
@@ -63,10 +58,7 @@ public class IndexControllerApiTest {
 
     @Test
     void getRequestToLoginEndpoint_shouldReturnLoginView() throws Exception {
-
-        MockHttpServletRequestBuilder request = get("/login");
-
-        mockMvc.perform(request)
+        mockMvc.perform(get("/login"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("login"))
                 .andExpect(model().attributeExists("loginRequest"));
@@ -74,92 +66,76 @@ public class IndexControllerApiTest {
 
     @Test
     void getRequestToLoginEndpointWithErrorParameter_shouldReturnLoginViewAndErrorMessageAttribute() throws Exception {
-
-        MockHttpServletRequestBuilder request = get("/login").param("error", "");
-
-        mockMvc.perform(request)
+        mockMvc.perform(get("/login").param("error", ""))
                 .andExpect(status().isOk())
                 .andExpect(view().name("login"))
                 .andExpect(model().attributeExists("loginRequest", "errorMessage"));
     }
 
-
     @Test
     void postRequestToRegisterEndpoint_happyPath() throws Exception {
-
-        MockHttpServletRequestBuilder request = post("/register")
-                .formField("username", "bob")
-                .formField("password", "123456")
-                .formField("email", "bob@bob.com")
-                .with(csrf());
-
-        mockMvc.perform(request)
+        mockMvc.perform(post("/register")
+                        .param("username", "bob")
+                        .param("password", "123456")
+                        .param("email", "bob@bob.com")
+                        .with(csrf()))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(redirectedUrl("/login"));
+
         verify(userService, times(1)).register(any());
     }
 
     @Test
     void postRequestToRegisterEndpointWhenUsernameAlreadyExist_thenRedirectToRegisterWithFlashParameter() throws Exception {
-
         doThrow(new UsernameAlreadyExistException("This username is already in use!"))
                 .when(userService)
                 .register(any(RegisterRequest.class));
 
-        MockHttpServletRequestBuilder request = post("/register")
-                .formField("username", "bob")
-                .formField("password", "123456")
-                .formField("email", "bob@bob.com")
-                .with(csrf());
-
-
-        mockMvc.perform(request)
+        mockMvc.perform(post("/register")
+                        .param("username", "bob")
+                        .param("password", "123456")
+                        .param("email", "bob@bob.com")
+                        .with(csrf()))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(redirectedUrl("/register"))
                 .andExpect(flash().attributeExists("usernameAlreadyExistMessage"));
+
         verify(userService, times(1)).register(any());
     }
 
     @Test
     void postRequestToRegisterEndpointWithInvalidData_returnRegisterView() throws Exception {
-
-        MockHttpServletRequestBuilder request = post("/register")
-                .formField("username", "")
-                .formField("password", "")
-                .formField("email", "bob@bob.com")
-                .with(csrf());
-
-        mockMvc.perform(request)
+        mockMvc.perform(post("/register")
+                        .param("username", "")
+                        .param("password", "")
+                        .param("email", "bob@bob.com")
+                        .with(csrf()))
                 .andExpect(status().isOk())
                 .andExpect(view().name("register"));
+
         verify(userService, never()).register(any());
     }
 
     @Test
     void getAuthenticatedRequestToHome_returnsHomeView() throws Exception {
+        UUID userId = UUID.randomUUID();
+        AuthenticationMetadata principal = new AuthenticationMetadata(userId, "User123", "123123", UserRole.USER, true);
 
         when(userService.getById(any())).thenReturn(aRandomUser());
 
-        UUID userId = UUID.randomUUID();
-        AuthenticationMetadata principal = new AuthenticationMetadata(userId, "User123", "123123", UserRole.USER, true);
-        MockHttpServletRequestBuilder request = get("/home")
-                .with(user(principal));
-
-
-        mockMvc.perform(request)
+        mockMvc.perform(get("/home").with(user(principal)))
                 .andExpect(status().isOk())
                 .andExpect(view().name("home"))
                 .andExpect(model().attributeExists("user"));
-        verify(userService, times(1)).getById(userId);
+
+        verify(userService, times(2)).getById(userId);
     }
 
     @Test
     void getUnauthenticatedRequestToHome_redirectToLogin() throws Exception {
-
-        MockHttpServletRequestBuilder request = get("/home");
-
-        mockMvc.perform(request)
+        mockMvc.perform(get("/home"))
                 .andExpect(status().is3xxRedirection());
+
         verify(userService, never()).getById(any());
     }
 }
